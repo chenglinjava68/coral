99070cebb3c74190ee8eda2fdade2a347ff2e14e
Ricky Fung <vricky.feng@gmail.com>
2018-06-26 19:20:04 +0800

    Update coral.sql

--- docs/sql/coral.sql
+++ docs/sql/coral.sql
@@ -12,8 +12,8 @@ create table `tb_segment`(
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `biz_tag` varchar(128) NOT NULL COMMENT '业务标识',
   `max_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '表示该biz_tag目前所被分配的ID号段的最大值',
-  `step` int(10) NOT NULL DEFAULT '1000' COMMENT '表示每次分配的号段长度'
-  `desc` varchar(256) NOT NULL DEFAULT 'default' COMMENT '应用名',
+  `step` int(10) NOT NULL DEFAULT '1000' COMMENT '表示每次分配的号段长度',
+  `description` varchar(256) NOT NULL DEFAULT 'default' COMMENT '应用名',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
   PRIMARY KEY (`id`),
